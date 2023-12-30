65391a881c1455ba2f0458c3bfea416bc50891e6
Silvia Saverino <silviasave89@gmail.com>
2023-12-30 18:51:56 +0000

    initial commit

---
+++ assets/js/data.js
@@ -0,0 +1,23 @@
+const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        id: 0,
+        price: 14,
+        image: "./assets/images/pizza.png",
+    },
+    {
+        name: "Burger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        image: "./assets/images/burger.png",
+        id: 1
+    },
+        {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        image: "./assets/images/beer.png",
+        id: 2
+    }
+]
