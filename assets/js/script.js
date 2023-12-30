9100676bc0f94a6a3be0c0a0d0eeee5bbf1748ea
Silvia Saverino <silviasave89@gmail.com>
2023-12-30 22:49:15 +0000

    render and style menu items

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,24 @@ pizzaBtn.addEventListener("click", function(){
     console.log("pizza btn working")
 })
 
-// console.log(menuArray)
+function renderMenu(menuArray){
+    return menuArray.map(menuDish => {
+        const {name, 
+            ingredients,
+            id, 
+            image, 
+            price,
+        } = menuDish
+        return `
+        <section class="menuItem">
+            <img src="${image}">
+            <div class="item-info">
+                <h2>${name}<span> £${price}</span></h2>
+                <h4>Ingredients: <span>${ingredients}<span></h4>
+            </div>
+        </section>
+        `
+    }).join('')
+   }
+   
+document.getElementById('menu').innerHTML = renderMenu(menuArray)
